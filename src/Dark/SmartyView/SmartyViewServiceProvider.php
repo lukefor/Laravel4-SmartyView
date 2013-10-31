27e2f1b83bc30718b32c7516f41aa829a5d35b0e
Dave Thomas <dave@monstergfx.com>
2013-10-30 21:34:19 -0700

    updated package name

--- src/Dark/SmartyView/SmartyViewServiceProvider.php
+++ src/Dark/SmartyView/SmartyViewServiceProvider.php
@@ -18,7 +18,7 @@ class SmartyViewServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		$this->package('dark/smartyView');
+		$this->package('monstergfx/smartyView');
 	}
 
 	/**
